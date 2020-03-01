97d36f903ed1b8c72f7b1c900cfbf483c1ad613d
spencerkevans <spencevans@gmail.com>
2020-02-29 22:24:31 -0500

    done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
@@ -9,13 +10,44 @@ class ApplicationController < Sinatra::Base
   	erb :index
   end
 
-  get 'recipes/new' do
+  get '/recipes/new' do
   	erb :new
+  end
+
+  post '/recipes' do
+  	@recipe = Recipe.create(params)
+  	if @recipe.save
+  		redirect to "/recipes/#{@recipe.id}"
+  	else
+  		
+  		erb :error
+  	end
   end
 
   get '/recipes/:id' do
   	@recipe = Recipe.find_by_id(params[:id])
   	erb :show
+  end
+
+  get '/recipes/:id/edit' do
+
+  	@recipe = Recipe.find_by_id(params[:id])
+  	erb :edit
+  end
+
+  patch '/recipes/:id' do
+  	@recipe = Recipe.find_by_id(params[:id])
+  	@recipe.name = params[:name]
+  	@recipe.ingredients = params[:ingredients]
+  	@recipe.cook_time = params[:cook_time]
+  	@recipe.save
+  	erb :show
+  end
+
+  delete '/recipes/:id' do
+  	@recipe = Recipe.find_by_id(params[:id])
+  	@recipe.delete
+  	redirect to '/recipes'
   end
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200229191214) do
+ActiveRecord::Schema.define(version: 20200229194209) do
 
   create_table "recipes", force: :cascade do |t|
-    t.string  "name"
-    t.string  "ingredients"
-    t.integer "cook_time"
+    t.string "name"
+    t.string "ingredients"
+    t.string "cook_time"
   end
 
 end
